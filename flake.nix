2ac8ccd3153e66a468f67b4d4d60ebda4350ef8f
jdirkse <jdirkse@mailme.not>
2025-11-08 12:02:44 +0100

    flake

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "A shell with Go";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        stdenv = pkgs.gccStdenv;
+        glibcStatic = pkgs.glibc.static;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          hardeningDisable = [ "fortify" ];
+          buildInputs = with pkgs; [
+            stdenv
+            glibc.static
+            delve
+            gcc
+            go
+            gotools
+            gopls
+            go-outline
+            gopkgs
+            gocode-gomod
+            godef
+            golint
+            nodejs_24
+          ];
+          packages = with pkgs; [
+            protobuf_32
+          ];
+          shellHook = ''
+            export PATH="$PATH:$(go env GOPATH)/bin"
+            npm set prefix ~/.npm-global
+            export PATH=$PATH:$HOME/.npm-global/bin
+          '';
+          GOROOT="${pkgs.go}/share/go";
+          GOPROXY="https://proxy.golang.org";
+          CGO_CFLAGS="-O2 -g -Wno-error";
+          CFLAGS="-I${pkgs.glibc.dev}/include";
+          LDFLAGS="-L${glibcStatic}/lib";
+          CGO_ENABLED=0;
+        };
+      });
+}
