7170c0ca2315d25b529e7fa9343af251ee46e6de
jdirkse <jdirkse@mailme.not>
2025-11-08 17:13:33 +0100

    refactor templates into separate package

---
+++ templates/templates.go
@@ -0,0 +1,50 @@
+package templates
+
+import (
+	"bytes"
+	_ "embed"
+	"text/template"
+)
+
+//go:embed query_builder.tmpl
+var queryBuilderTemplate string
+
+// QueryBuilderData contains the data for rendering the query builder template
+type QueryBuilderData struct {
+	BuilderName       string
+	StructName        string
+	TableName         string
+	Columns           []Column
+	NonPrimaryColumns []Column
+	PrimaryKey        string
+	PrimaryKeyField   string
+	PrimaryKeyType    string
+}
+
+// Column represents template column data
+type Column struct {
+	Name       string
+	FieldName  string
+	GoType     string
+	IsNullable bool
+}
+
+// RenderQueryBuilder renders the query builder template with the given data
+func RenderQueryBuilder(data QueryBuilderData) (string, error) {
+	// Create template with custom functions
+	funcMap := template.FuncMap{
+		"add": func(a, b int) int { return a + b },
+	}
+
+	t, err := template.New("queryBuilder").Funcs(funcMap).Parse(queryBuilderTemplate)
+	if err != nil {
+		return "", err
+	}
+
+	var buf bytes.Buffer
+	if err := t.Execute(&buf, data); err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
+}
